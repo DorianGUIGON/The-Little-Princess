c607f1f5fc3567c0c3e57a6212bf0c971091faee
MaxenceRoques <maxence.roques@etu.univ-cotedazur.fr>
2025-04-10 22:31:27 +0200

    Voiture

--- Assets/TheArenaOfSpeedAndPurpose/Scripts/CarMovement.cs
+++ Assets/TheArenaOfSpeedAndPurpose/Scripts/CarMovement.cs
@@ -9,24 +9,44 @@ public class CarMovement : MonoBehaviour
     public float backwardMoveSpeed;
     public float steerSpeed;
 
+    private bool isSlippery = false;
+    public float slipperyMultiplier = 1.5f;
+    public float slipperySteerReduction = 0.5f;
+
     public void SetInputs(Vector2 input)
     {
         this.input = input;
     }
 
-    void FixedUpdate() // Apply physics here
+    void FixedUpdate()
     {
-        // Accelerate
-        float speed = input.y > 0 ? forwardMoveSpeed : backwardMoveSpeed;
+        float speedMultiplier = isSlippery ? 1.5f : 1f;
+        float steerMultiplier = isSlippery ? 1.5f : 1f;
+
+        // Accélération
+        float speed = (input.y > 0 ? forwardMoveSpeed : backwardMoveSpeed) * speedMultiplier;
         if (input.y == 0) speed = 0;
-        rg.AddForce(this.transform.forward * speed, ForceMode.Acceleration);
-        // Steer
-        float rotation = input.x * steerSpeed * Time.fixedDeltaTime;
+        rg.AddForce(transform.forward * speed, ForceMode.Acceleration);
+
+        float rotation = input.x * steerSpeed * steerMultiplier * Time.fixedDeltaTime;
         transform.Rotate(0, rotation, 0, Space.World);
     }
 
-    public float GetSpeed()
+    void OnTriggerEnter(Collider other)
     {
-        return rg.velocity.magnitude;
+        if (other.CompareTag("Slippery"))
+        {
+            Debug.Log("🚗💨 Ça glisse et accélère !");
+            isSlippery = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Slippery"))
+        {
+            Debug.Log("Retour à la normale");
+            isSlippery = false;
+        }
     }
 }
